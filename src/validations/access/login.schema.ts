bcdc489c1f7c6f38f67055644ce6dc2828477c75
Dien Cap <baodien1405@gmail.com>
2024-08-04 00:18:43 +0700

    fix: Remove rule validation password

--- src/validations/access/login.schema.ts
+++ src/validations/access/login.schema.ts
@@ -3,5 +3,5 @@ import { Login } from '@/types'
 
 export const loginSchema = Joi.object<Login>({
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+  password: Joi.string().min(6).required()
 })
